078ed3f8eb8ed632f8d57ed4f9ab34217ade0d85
caruli <caruli@gmail.com>
2022-03-03 15:44:58 +0100

    sd

--- goods.c
+++ goods.c
@@ -2,7 +2,7 @@
 #include "tc.h"
 
 float sum(float mbank, float tipsport, float bank_notes, float coins, float care_benefit,float debt);
-float mbank = 3284.49, tipsport = 0.80,bank_notes = 270.00, coins=2.30, care_benefit= 0.00, debt=0.00;
+float mbank = 3284.49, tipsport = 430.35,bank_notes = 670.00, coins=2.30, care_benefit= 1836.03, debt=0.00;
 int main() {
   //int x,y;
   //int row,col;
