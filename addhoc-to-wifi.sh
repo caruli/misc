e55b0d2e05713602e7e4c4af2d4651bded3b8261
caruli <caruli@gmail.com>
2022-04-03 18:19:25 +0200

    ds

--- addhoc-to-wifi.sh
+++ addhoc-to-wifi.sh
@@ -2,5 +2,7 @@
 sudo cp /etc/network/wifi-interfaces  /etc/network/interfaces
 sudo systemctl enable dhcpcd.service
 sudo systemctl start  dhcpcd.service
-sudo systemctl status dhcpcd.service
-sudo ifconfig wlan0 10.3.141.50 up
+#sudo systemctl status dhcpcd.service
+sudo ifconfig wlan0 down
+sudo ifconfig wlan0 up
+
--- wifi-to-add-hoc.sh
+++ wifi-to-add-hoc.sh
@@ -3,5 +3,5 @@ sudo cp /etc/network/adhoc-interfaces  /etc/network/interfaces
 sudo systemctl stop dhcpcd.service
 sudo systemctl disable dhcpcd.service
 sudo systemctl status dhcpcd.service
-sudo ifconfig wlan0 192.168.1.1 up
+sudo ifconfig wlan0 192.168.1.1 netmask 255.255.255.0 up
 #sudo reboot
