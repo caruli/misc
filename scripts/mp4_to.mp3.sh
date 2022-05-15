92576ea1df4bc2261b83ddc18f9f6104c7806f1b
caruli <caruli@gmail.com>
2022-05-15 21:28:28 +0200

    sds

--- scripts/mp4_to.mp3.sh
+++
@@ -1,7 +0,0 @@
-y#!/bin/bash
-MP4FILE=$(ls ~/Music/ | grep .mp4)
-for filename in $MP4FILE
-do 
- name=`echo "$filename" | sed -e "s/.mp4$//g"`
- ffmpeg -i ~/Music/$filename -b:a 192K -vn ~/Music/$name.mp3
-done
